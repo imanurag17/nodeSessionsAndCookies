05a13e427471cba3c45c8b942b749f708415e58c
imanurag17 <anuraag.cas@gmail.com>
2025-09-23 12:44:39 +0530

    use session middleware and connect session to database

--- app.js
+++ app.js
@@ -3,38 +3,54 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://anurag17:xyzxyz%40123@cluster0.iqstawb.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'session'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(session({ secret: 'My Secret', resave: false, saveUninitialized: false, store: store }))
+
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
-      req.user = user;
-      next();
+      req.user = user
+      next()
     })
     .catch(err => console.log(err));
-});
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://anurag17:xyzxyz%40123@cluster0.iqstawb.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0',
+    MONGODB_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -44,14 +60,14 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
+          name: 'alpha',
           email: 'max@test.com',
           cart: {
             items: []
           }
         });
         console.log('connected');
-        
+
         user.save();
       }
     });
